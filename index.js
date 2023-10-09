4193fea51987706a8b76215dc9d2b4c1f79465d4
Okko <142894633+eoovve@users.noreply.github.com>
2023-10-09 22:09:27 +0800

    Circleci
    Circleci

---
+++ index.js
@@ -0,0 +1,80 @@
+const { spawn } = require('child_process');
+const port = process.env.SERVER_PORT || process.env.PORT || 3000;
+const http = require('http');
+const fs = require('fs');
+
+const startScriptPath = './start.sh';
+const listFilePath = 'list.txt';
+const subFilePath = 'sub.txt';
+
+try {
+  fs.chmodSync(startScriptPath, 0o777);
+  console.log(`Authorization Successful: ${startScriptPath}`);
+} catch (error) {
+  console.error(`Authorization Failed: ${error}`);
+}
+
+const startScript = spawn(startScriptPath);
+
+startScript.stdout.on('data', (data) => {
+  console.log(`exports: ${data}`);
+});
+
+startScript.stderr.on('data', (data) => {
+  console.error(`${data}`);
+});
+
+startScript.on('error', (error) => {
+  console.error(`Launch script error: ${error}`);
+  process.exit(1); 
+});
+
+startScript.on('close', (code) => {
+  console.log(`Child process exit, exit code ${code}`);
+});
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200);
+    res.end('hello world');
+  } else if (req.url === '/list') {
+    fs.readFile(listFilePath, 'utf8', (error, data) => {
+      if (error) {
+        res.writeHead(500);
+        res.end('Error reading file');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/sub') {
+    fs.readFile(subFilePath, 'utf8', (error, data) => {
+      if (error) {
+        res.writeHead(500);
+        res.end('Error reading file');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// 3分钟后自动删除list和sub文件
+setTimeout(() => {
+  if (fs.existsSync(listFilePath)) {
+    fs.unlinkSync(listFilePath);
+    console.log(`Deleted ${listFilePath}`);
+  }
+  if (fs.existsSync(subFilePath)) {
+    fs.unlinkSync(subFilePath);
+    console.log(`Deleted ${subFilePath}`);
+  }
+}, 180000);
